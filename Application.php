5a1cc2377d60a4f83e322321d43fdcc1a593f1d6
Duy Tuan <duytuanvn1986@gmail.com>
2023-10-15 14:13:56 +0200

    initial commit

---
+++ Application.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace app\core;
+
+use app\core\db\Database;
+use app\core\db\DbModel;
+use app\models\User;
+
+class Application
+{
+
+  public static string $ROOT_DIR;
+
+  public string $layout = 'main';
+  public $userClass;
+  public Router $router;
+  public Request $request;
+  public Response $response;
+  public Session $session;
+  public Database $db;
+  public static Application $app;
+  public ?Controller $controller = null;
+  public ?UserModel $user;
+  public View $view;
+
+
+
+  public function __construct($routePath, array $config)
+  {
+    $this->userClass = new User();
+    self::$ROOT_DIR = $routePath;
+    self::$app = $this;
+    $this->request = new Request();
+    $this->response = new Response();
+    $this->session = new Session();
+    $this->router = new Router($this->request, $this->response);
+    $this->db = new Database($config['db']);
+    $this->view = new View();
+
+    $primaryValue = $this->session->get('user');
+    if ($primaryValue) {
+      $primaryKey = $this->userClass::primaryKey();
+      $this->user = $this->userClass->findOne([$primaryKey =>  $primaryValue]);
+    } else {
+      $this->user = null;
+    }
+  }
+
+  public function run()
+  {
+    try {
+      echo $this->router->resolve();
+    } catch (\Exception $e) {
+      $this->response->setStatusCode($e->getCode());
+      echo $this->view->renderView('_error', [
+        'exception' => $e
+      ]);
+    }
+  }
+
+
+  public function getController()
+  {
+    return $this->controller;
+  }
+
+
+  public function setController(Controller $controller)
+  {
+    $this->controller = $controller;
+  }
+
+  public function login(UserModel $user)
+  {
+    $this->user = $user;
+    $primaryKey = $user->primaryKey();
+    $primaryValue = $user->{$primaryKey};
+    $this->session->set('user', $primaryValue);
+    return true;
+  }
+
+  public function logout()
+  {
+    $this->user = null;
+    $this->session->remove('user');
+  }
+
+  public static function isGuest()
+  {
+    return !self::$app->user;
+  }
+}
---
+++ Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\core;
+
+use app\core\Application;
+use app\core\middlewares\BaseMiddleware;
+
+class Controller
+{
+
+  public string $layout = 'main';
+  protected array $middlewares = [];
+  public string $action = '';
+
+  public function setLayout($layout)
+  {
+    $this->layout = $layout;
+  }
+
+
+  public function render($view, $params = [])
+  {
+    return Application::$app->view->renderView($view, $params);
+  }
+
+  public function registerMiddleware(BaseMiddleware $middleware)
+  {
+    $this->middlewares[] = $middleware;
+  }
+
+
+
+
+  public function getMiddlewares()
+  {
+    return $this->middlewares;
+  }
+}
